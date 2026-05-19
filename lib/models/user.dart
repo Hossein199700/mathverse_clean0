f28028b7f83351aca9285a1f57ee98844b1edbf5
Hossein199700 <alirahmani999877@gmail.com>
2026-05-19 16:55:24 +0330

    Update user.dart

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,34 +2,39 @@ class AppUser {
   String username;
   String password;
   String bio;
+
   int score;
-  int cup;
+  int xp;
+  int level;
+  String league;
 
   AppUser({
     required this.username,
     required this.password,
     this.bio = "",
     this.score = 0,
-    this.cup = 0,
+    this.xp = 0,
+    this.level = 1,
+    this.league = "Bronze",
   });
 
-  Map<String, dynamic> toMap() {
-    return {
-      "username": username,
-      "password": password,
-      "bio": bio,
-      "score": score,
-      "cup": cup,
-    };
-  }
+  Map<String, dynamic> toMap() => {
+    "username": username,
+    "password": password,
+    "bio": bio,
+    "score": score,
+    "xp": xp,
+    "level": level,
+    "league": league,
+  };
 
-  static AppUser fromMap(Map data) {
-    return AppUser(
-      username: data["username"],
-      password: data["password"],
-      bio: data["bio"] ?? "",
-      score: data["score"] ?? 0,
-      cup: data["cup"] ?? 0,
-    );
-  }
+  static AppUser fromMap(Map m) => AppUser(
+    username: m["username"],
+    password: m["password"],
+    bio: m["bio"] ?? "",
+    score: m["score"] ?? 0,
+    xp: m["xp"] ?? 0,
+    level: m["level"] ?? 1,
+    league: m["league"] ?? "Bronze",
+  );
 }
