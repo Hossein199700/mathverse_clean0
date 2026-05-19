d7f64fc04b6ee0a450cb7209d270b2529d300d42
Hossein199700 <alirahmani999877@gmail.com>
2026-05-19 16:24:08 +0330

    Update user.dart

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,19 +1,25 @@
 class AppUser {
   String username;
   String password;
+  String bio;
   int score;
+  int cup;
 
   AppUser({
     required this.username,
     required this.password,
+    this.bio = "",
     this.score = 0,
+    this.cup = 0,
   });
 
   Map<String, dynamic> toMap() {
     return {
       "username": username,
       "password": password,
+      "bio": bio,
       "score": score,
+      "cup": cup,
     };
   }
 
@@ -21,7 +27,9 @@ class AppUser {
     return AppUser(
       username: data["username"],
       password: data["password"],
+      bio: data["bio"] ?? "",
       score: data["score"] ?? 0,
+      cup: data["cup"] ?? 0,
     );
   }
 }
