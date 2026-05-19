3fcee031bacbace218b15f45830bf57d4ab66bc0
Hossein199700 <alirahmani999877@gmail.com>
2026-05-19 16:20:07 +0330

    Create question.dart

---
+++ lib/models/question.dart
@@ -0,0 +1,11 @@
+class Question {
+  String text;
+  int answer;
+  String level; // medium / hard
+
+  Question({
+    required this.text,
+    required this.answer,
+    required this.level,
+  });
+}
